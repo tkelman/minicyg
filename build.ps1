20b92ca26f41d490568af4dbac43e594eb989fce
Tony Kelman <tony@kelman.net>
2017-01-31 05:22:14 -0500

    Use cygbuild, to avoid dealing with existing appveyor cygwin install

--- build.ps1
+++ build.ps1
@@ -3,13 +3,11 @@ $bits = "64"
 $arch = "x86_$bits".Replace("x86_32", "i686")
 $setup = "setup-$arch.exe".Replace("i686", "x86")
 
-ls C:\\
-
-mkdir -Force C:\\cygwin$bits | Out-Null
+mkdir -Force C:\\cygbuild$bits | Out-Null
 (new-object net.webclient).DownloadFile(
-  "http://cygwin.com/$setup", "C:\\cygwin$bits\\$setup")
-& "C:\\cygwin$bits\\$setup" -q -n -R C:\\cygwin$bits -l C:\\cygwin$bits\\packages `
+  "http://cygwin.com/$setup", "C:\\cygbuild$bits\\$setup")
+& "C:\\cygbuild$bits\\$setup" -q -n -R C:\\cygbuild$bits -l C:\\cygbuild$bits\\packages `
   -s http://mirrors.mit.edu/cygwin -g -P "make,gcc,cygport" | Where-Object `
   -FilterScript {$_ -notlike "Installing file *"} | Write-Output
-& "C:\\cygwin$bits\\bin\\sh" -lc "echo ok && \\
+& "C:\\cygbuild$bits\\bin\\sh" -lc "echo ok && \\
   echo ok"
