3be7c02abd13723146869c5a64601cb03b91d052
Tony Kelman <tony@kelman.net>
2017-01-31 07:27:34 -0500

    Check out specific commits
    of sbase and libsixel
    
    put minicyg contents in place

--- build.ps1
+++ build.ps1
@@ -18,8 +18,23 @@ mkdir -Force C:\\cygbuild$bits | Out-Null
 & "C:\\cygbuild$bits\\$setup" -q -n -R C:\\cygbuild$bits -l C:\\cygbuild$bits\\packages `
   -s http://mirrors.mit.edu/cygwin -g -P "git,cygport" | Where-Object `
   -FilterScript {$_ -notlike "Installing file *"} | Write-Output
-& "C:\\cygbuild$bits\\bin\\sh" -lc "git clone git://git.suckless.org/sbase && \\
-  cd sbase && make -j3 && git clone https://github.com/saitoha/libsixel ../libsixel && \\
-  cd ../libsixel && ./configure && make -j3 && cd /usr/src/coreutils-* && \\
-  sed -i 's/CYGCONF_ARGS=`"/CYGCONF_ARGS=`"--disable-nls /' coreutils.cygport && \\
-  cygport coreutils.cygport all && echo ok"
+& "C:\\cygbuild$bits\\bin\\sh" -lc "git clone git://git.suckless.org/sbase /usr/src/sbase && \\
+  cd /usr/src/sbase && git checkout 9ab1478f1eb && make -j3 && \\
+  git clone https://github.com/saitoha/libsixel /usr/src/libsixel && \\
+  cd /usr/src/libsixel && git checkout 8c376f5ad03 && ./configure && make -j3 && \\
+  cd /usr/src/coreutils-* && \\
+  sed -i 's/CYGCONF_ARGS=\`"/CYGCONF_ARGS=\`"--disable-nls /' coreutils.cygport && \\
+  cygport coreutils.cygport all && cygcheck coreutils-*/inst/usr/bin/stty.exe && \\
+  cygcheck /usr/bin/stty.exe && mkdir -p /usr/src/minicyg/bin && \\
+  cp /bin/cygwin1.dll /bin/mintty.exe /bin/dash.exe /usr/src/minicyg/bin && \\
+  cp /usr/src/sbase/*.exe /usr/src/libsixel/converters/img2sixel.exe /usr/src/minicyg/bin && \\
+  cp /usr/src/coreutils-*/inst/usr/bin/stty.exe /usr/src/minicyg/bin && \\
+  for i in Cygwin mintty dash coreutils; do \\
+    mkdir -p /usr/src/minicyg/share/doc/$i && \\
+    cp /usr/share/doc/$i/COPYING /usr/src/minicyg/share/doc/$i && \\
+    cp /usr/share/doc/$i/*LICENSE* /usr/src/minicyg/share/doc/$i \\
+  done && \\
+  for i in sbase libsixel; do \\
+    mkdir -p /usr/src/minicyg/share/doc/$i && \\
+    cp /usr/src/$i/LICENSE* /usr/src/minicyg/share/doc/$i \\
+  done && echo ok"
