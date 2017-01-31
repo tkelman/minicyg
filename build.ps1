e7929b58f7e0f251e3c5cf01618edf453689d5a4
Tony Kelman <tony@kelman.net>
2017-01-31 09:08:14 -0500

    try cachine cygwin downloads

--- build.ps1
+++ build.ps1
@@ -12,10 +12,10 @@ mkdir -Force C:\\cygbuild$bits | Out-Null
 # TODO: use cygwin time machine for build reproducibility
 # download sources (-I) for coreutils and make
 # (maybe apt-cyg could be used to avoid downloading -src for the entire base system?)
-& "C:\\cygbuild$bits\\$setup" -q -n -R C:\\cygbuild$bits -l C:\\cygbuild$bits\\packages `
+& "C:\\cygbuild$bits\\$setup" -q -n -R C:\\cygbuild$bits -l C:\\cygdl$bits `
   -s http://mirrors.mit.edu/cygwin -g -I -P "coreutils,make" | Where-Object `
   -FilterScript {$_ -notlike "Installing file *"} | Write-Output
-& "C:\\cygbuild$bits\\$setup" -q -n -R C:\\cygbuild$bits -l C:\\cygbuild$bits\\packages `
+& "C:\\cygbuild$bits\\$setup" -q -n -R C:\\cygbuild$bits -l C:\\cygdl$bits `
   -s http://mirrors.mit.edu/cygwin -g -P "git,cygport,gnupg,p7zip" | Where-Object `
   -FilterScript {$_ -notlike "Installing file *"} | Write-Output
 & "C:\\cygbuild$bits\\bin\\sh" -lc "git clone git://git.suckless.org/sbase /usr/share/doc/sbase 2>&1 && \\
