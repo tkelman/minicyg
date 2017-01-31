66b8a618d69a18b016b6a1420454893e613a0bf1
Tony Kelman <tony@kelman.net>
2017-01-31 05:43:29 -0500

    use -I for sources
    and remove cygwin if already installed

--- build.ps1
+++ build.ps1
@@ -3,11 +3,14 @@ $bits = "64"
 $arch = "x86_$bits".Replace("x86_32", "i686")
 $setup = "setup-$arch.exe".Replace("i686", "x86")
 
+if (Test-Path "C:\\cygbuild$bits") {
+  rm -Recurse -Force C:\\cygbuild$bits
+}
 mkdir -Force C:\\cygbuild$bits | Out-Null
 (new-object net.webclient).DownloadFile(
   "http://cygwin.com/$setup", "C:\\cygbuild$bits\\$setup")
 & "C:\\cygbuild$bits\\$setup" -q -n -R C:\\cygbuild$bits -l C:\\cygbuild$bits\\packages `
-  -s http://mirrors.mit.edu/cygwin -g -P "make,gcc,cygport" | Where-Object `
+  -s http://mirrors.mit.edu/cygwin -g -I -P "git,cygport,coreutils" | Where-Object `
   -FilterScript {$_ -notlike "Installing file *"} | Write-Output
 & "C:\\cygbuild$bits\\bin\\sh" -lc "echo ok && \\
-  echo ok"
+  ls -al /usr/src"
